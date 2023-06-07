e04a8f160447c00f5c723989c722e8a797ed7440
zhangjingyuan <891403766@qq.com>
2023-06-07 17:23:24 +0800

    fix:页面图片修改

--- src/views/system/blank/index.vue
+++ src/views/system/blank/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width: 86%;">
           <div style="font-size:32px; font-weight: bold; color:rgb(51, 150, 209); margin-top: 10%;text-align: center;">
-             <img src="./img/logo_MJJC.jpg" width="22%">
+             <!-- <img src="./img/logo_MJJC.jpg" width="22%"> -->
               </br>
                  <i style="font-size:28px;" :class="'ibps-icon-'+$route.query.icon"></i>
                   {{$route.query.name}}
