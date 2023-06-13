90bdbd181330ec13c2b83bf380a2a1c98c918515
liujiayin <szjbd_ljy@126.com>
2023-06-13 17:26:46 +0800

    add：文件类型管理

---
+++ src/views/platform/cat/type/fileType.vue
@@ -0,0 +1,15 @@
+<template>
+  <manage categoryKey="FILE_TYPE" />
+</template>
+<script>
+import Manage from './manage'
+export default {
+  components: {
+    Manage
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
