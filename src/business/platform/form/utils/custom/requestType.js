49590bb2c2cf3c9b93b0916693efa1de0636ecb4
cfort <cfortwang@gmail.com>
2023-08-18 16:01:54 +0800

    修改通用查询接口

--- src/business/platform/form/utils/custom/requestType.js
+++ src/business/platform/form/utils/custom/requestType.js
@@ -1,6 +1,7 @@
 // 通用请求，增删改查
 export const normal = {
-    sql: 'encipher',
+    // sql: 'encipher',
+    sql: 'general',
     delete: 'batchDelete',
     add: 'addDataContextTable',
     update: 'updatesBatchContextTable'
